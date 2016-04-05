755089e1256166c71b55bafcb87f7b9cedead144
Yevgeniy Brikman <brikis98@gmail.com>
2016-04-05 17:33:48 +0200

    Better logging in User Data

--- terraform-example/user-data.sh
+++ terraform-example/user-data.sh
@@ -3,6 +3,11 @@
 
 set -e
 
+# Send the log output from this script to user-data.log, syslog, and the console
+# From: https://alestic.com/2010/12/ec2-user-data-output/
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+echo "Starting Ruby on Rails app on port ${port}"
 cd /home/ubuntu/example-rails-app
 nohup rails s --port=${port} 2>&1
 
