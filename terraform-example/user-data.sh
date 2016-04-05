40f986c0dbd40e52020018951ccf9eb23160fde1
Yevgeniy Brikman <brikis98@gmail.com>
2016-04-05 18:23:21 +0200

    Add binding for rails s command

--- terraform-example/user-data.sh
+++ terraform-example/user-data.sh
@@ -9,5 +9,5 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 echo "Starting Ruby on Rails app on port ${port} and piping all log output to syslog"
 cd /home/ubuntu/example-rails-app
-nohup rails s --port=${port} 2>&1 | logger &
+nohup rails s --port=${port} --binding=0.0.0.0 2>&1 | logger &
 
