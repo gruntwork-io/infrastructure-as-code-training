7c868a46331e6288fc950ee906fb3bd63512c84a
Josh Padnick <josh.padnick@gmail.com>
2017-04-04 13:21:05 -0700

    Trivial formatting update.

--- terraform-example-basic/main.tf
+++ terraform-example-basic/main.tf
@@ -6,11 +6,10 @@ provider "aws" {
 # Create an EC2 instance
 resource "aws_instance" "example" {
   # AMI ID for Amazon Linux AMI 2016.03.0 (HVM)
-  ami = "ami-08111162"
+  ami           = "ami-08111162"
   instance_type = "t2.micro"
 
   tags {
     Name = "example"
   }
 }
-
