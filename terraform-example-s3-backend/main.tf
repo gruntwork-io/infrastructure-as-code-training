376ef36d869851ebaf813fb51fdc733db31d58de
Josh Padnick <josh.padnick@gmail.com>
2017-06-01 15:14:34 -0700

    Update s3 backend example to use AMI search.

--- terraform-example-s3-backend/main.tf
+++ terraform-example-s3-backend/main.tf
@@ -1,9 +1,10 @@
 # Configure the Terraform backend
 terraform {
   backend "s3" {
+    # Be sure to change this bucket name and region to match an S3 Bucket you have already created!
     bucket = "gruntwork-iac-training"
+    region = "us-west-2"
     key    = "terraform.tfstate"
-    region = "us-east-1"
   }
 }
 
@@ -12,17 +13,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "ec2_name" {
-  description = "name of my EC2 Instance"
-  type        = "string"
-  default     = "josh"
+# Get the latest Amazon Linux AMI
+data "aws_ami" "amzn_linux" {
+  most_recent = true
+
+  filter {
+    name = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
 }
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  # AMI ID for Amazon Linux AMI 2016.03.0 (HVM)
-  ami           = "ami-08111162"
+  ami = "${data.aws_ami.amzn_linux.image_id}"
   instance_type = "t2.micro"
+  key_name = "${var.key_pair_name}"
 
   tags {
     Name = "${var.ec2_name}"
---
+++ terraform-example-s3-backend/vars.tf
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+# ---------------------------------------------------------------------------------------------------------------------
+# MODULE PARAMETERS
+# These variables are expected to be passed in by the operator
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "ec2_name" {
+  description = "The name of the EC2 Instance."
+}
+
+variable "key_pair_name" {
+  description = "The name of a Key Pair that you've created in AWS and have saved on your computer. You will be able to use this Key Pair to SSH to the EC2 instance."
+}
