530a8effadcf2cfa85e7b65da395bffef7fc2a45
Oreoluwa Agunbiade <agunbiade.oreoluwa@gmail.com>
2019-05-14 17:02:34 +0100

    Revert "Fixes invalid ami-id error by using newer linux ami"

--- terraform-example-basic/main.tf
+++ terraform-example-basic/main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  # AMI ID for Amazon Linux 2018.03.0 (HVM)
-  ami           = "ami-0cd3dfa4e37921605"
+  # AMI ID for Amazon Linux AMI 2016.03.0 (HVM)
+  ami           = "ami-08111162"
   instance_type = "t2.micro"
 
   tags {
