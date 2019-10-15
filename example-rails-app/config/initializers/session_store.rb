786e0d4aee1682a20efe7f20aef6433b75c50697
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-10-14 17:24:16 -0700

    Update rails to 4.2.x

--- example-rails-app/config/initializers/session_store.rb
+++ example-rails-app/config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ExampleRailsApp::Application.config.session_store :cookie_store, key: '_example-rails-app_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# ExampleRailsApp::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_example_rails_app_session'
