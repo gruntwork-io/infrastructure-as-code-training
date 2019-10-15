2d3637033fd0088a3a51ec048a5985b1f0793f8f
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-10-14 17:07:36 -0700

    Add another module block to demonstrate multiple module calls

--- terraform-example-full/outputs.tf
+++ terraform-example-full/outputs.tf
@@ -1,4 +1,9 @@
-# Output EC2 instance URL from the example_rails_app module
-output "url" {
-  value = module.example_rails_app.url
+# Output EC2 instance URL from the example_rails_app_stage module
+output "stage_url" {
+  value = module.example_rails_app_stage.url
+}
+
+# Output EC2 instance URL from the example_rails_app_prod module
+output "prod_url" {
+  value = module.example_rails_app_prod.url
 }
